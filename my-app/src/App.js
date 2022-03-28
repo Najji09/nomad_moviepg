25cacb778faeb95b45a05fecfc67a6ad93f7b5a3
Najji09 <n96h09j09@outlook.kr>
2022-03-28 16:51:57 +0900

    v0.5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Jsx from './Basics/jsx';
 import State from './Basics/State';
+import StateProps from './Basics/StateProps';
 
 function App() {
   return (
     <div className="App">
       <Jsx />
       <State />
+      <StateProps />
     </div>
   );
 }
--- my-app/src/Basics/State.js
+++ my-app/src/Basics/State.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function State() {
+function Time() {
   const [amount, setAmount] = React.useState('');
   const onChange = (event) => {
     setAmount(event.target.value);
@@ -16,7 +16,7 @@ function State() {
 
   return (
     <div>
-      <h1> Super Converter</h1>
+      <h3> Minutes and Hours</h3>
       <div>
         <label htmlFor="minutes">Minutes</label>
         <input
@@ -32,7 +32,7 @@ function State() {
       <div>
         <label htmlFor="hours">Hours</label>
         <input
-          value={flipped ? amount : Math.round(amount / 60)}
+          value={flipped ? amount : Math.round((amount / 60) * 10) / 10}
           id="hours"
           placeholder="Hours"
           type="
@@ -42,9 +42,92 @@ function State() {
         />
       </div>
       <button onClick={reset}>Reset button</button>
-      <button onClick={onFlip}>Flip?</button>
+      <button onClick={onFlip}>
+        {flipped ? 'Type Minutes' : 'Type Hours'}
+      </button>
     </div>
   );
 }
+function Meter() {
+  const [amount, setAmount] = React.useState('');
+  const onChange = (event) => {
+    setAmount(event.target.value);
+  };
+  const reset = () => {
+    setAmount('');
+  };
+  const [flipped, SetFlipped] = React.useState(false);
+  const onFlip = () => {
+    reset();
+    SetFlipped((current) => !current);
+  };
+
+  return (
+    <div>
+      <h3>Miles and Killometer</h3>
+      <div>
+        <label htmlFor="miles">Miles</label>
+        <input
+          id="miles"
+          placeholder="Miles"
+          type="number"
+          value={flipped ? Math.round((amount / 1.6) * 10) / 10 : amount}
+          onChange={onChange}
+          disabled={flipped}
+        />
+        <h4>
+          You want to convert{amount} {flipped ? 'Killometers' : 'Miles'}
+        </h4>
+      </div>
+      <div>
+        <label htmlFor="km">Killometers</label>
+        <input
+          value={flipped ? amount : Math.round(amount * 1.6 * 10) / 10}
+          id="km"
+          placeholder="Killometers"
+          type="
+        number"
+          disabled={!flipped}
+          onChange={onChange}
+        />
+      </div>
+      <button onClick={reset}>Reset button</button>
+      <button onClick={onFlip}>
+        {flipped ? 'Type Miles' : 'Type Killometers'}
+      </button>
+    </div>
+  );
+}
+
+const State = () => {
+  const [index, setIndex] = React.useState('0');
+  const onSelect = (event) => {
+    setIndex(event.target.value);
+    converterSwitch(index);
+  };
+  const converterSwitch = (a) => {
+    console.log(a);
+    switch (a) {
+      case '0':
+        return <h1>Select the unit</h1>;
+      case '1':
+        return <Time />;
+
+      case '2':
+        return <Meter />;
+    }
+  };
+  return (
+    <div>
+      <h1> Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="0">Select your unit</option>
+        <option value="1">Minutes and Hours</option>
+        <option value="2">Miles and Killometers</option>
+      </select>
+      {converterSwitch(index)}
+    </div>
+  );
+};
 
 export default State;
