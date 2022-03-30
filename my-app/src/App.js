f992d648b6452c9ee232a90f9c1b053ee6ce32c2
Najji09 <100065117+Najji09@users.noreply.github.com>
2022-03-30 17:15:51 +0900

    v0.8

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,30 @@ import State from './Basics/State';
 import Props from './Basics/Props';
 import Effect from './Basics/Effect';
 import styles from './App.module.css';
+import EffectCleanup from './Basics/EffectCleanup';
+import { useState } from 'react';
 // import StateProps from './Basics/StateProps';
 
 function App() {
+  const [basics, showBasics] = useState(false);
+  const onClick = () => {
+    showBasics((prev) => !prev);
+  };
   return (
     <div className="App">
-      <h1 className={styles.title}>Hi</h1>
-      <Jsx />
-      <State />
-      <Props />
-      <Effect />
-      {/* <StateProps /> */}
+      <button onClick={onClick}>
+        {basics === true ? 'Hide Basics' : 'Show Basics'}
+      </button>
+      {basics === true ? (
+        <div>
+          <h1 className={styles.title}>Hi</h1>
+          <Jsx />
+          <State />
+          <Props />
+          <Effect />
+          <EffectCleanup />
+        </div>
+      ) : null}
     </div>
   );
 }
--- my-app/src/Basics/Effect.js
+++ my-app/src/Basics/Effect.js
@@ -3,18 +3,33 @@ import { useEffect, useState } from 'react';
 
 const Effect = () => {
   console.log('Run all the time');
-  function onlyOnce() {
-    console.log('Run only Once');
-  }
+  const [keyword, setKeyword] = useState('');
+  const onKeywordChange = (e) => {
+    setKeyword(e.target.value);
+  };
   useEffect(() => {
     console.log('Call API...');
   }, []);
+  useEffect(() => {
+    if (keyword.length >= 5) {
+      console.log('Search for', keyword);
+    }
+  }, [keyword]);
   const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log('I run when keyword & counter change');
+  }, [keyword, counter]);
   const onClick = () => {
     setCounter((prev) => prev + 1);
   };
   return (
     <div>
+      <input
+        onChange={onKeywordChange}
+        value={keyword}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me!</button>
     </div>
