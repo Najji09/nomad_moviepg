8dcf387e4fe4719bb5c9b29cdb825e59186515d4
Najji09 <n96h09j09@outlook.kr>
2022-04-11 13:46:35 +0900

    publish

--- my-app/src/Routes/Detail.js
+++ my-app/src/Routes/Detail.js
@@ -1,12 +1,38 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import MovieInfo from '../Components/MovieInfo';
 
 function Detail() {
   const { id } = useParams();
-  console.log(id);
+  const [movie, setMovie] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  console.log(movie);
   return (
     <div>
-      <h1>Detail</h1>
-      <p></p>
+      {loading ? (
+        <h1 className="loading">Loading</h1>
+      ) : (
+        <MovieInfo
+          title={movie.title}
+          medium_cover_image={movie.medium_cover_image}
+          rating={movie.rating}
+          genre={movie.genres}
+          year={movie.year}
+          runtime={movie.runtime}
+          description_full={movie.description_full}
+        />
+      )}
     </div>
   );
 }
